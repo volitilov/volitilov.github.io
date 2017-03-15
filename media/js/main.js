49b6689f09bccf3dc4c088e13447dc767860b130
volitilov <volitilov@gmail.com>
2017-03-15 21:11:44 +0300

    hosting website

---
+++ media/js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', '/media/assets/particles/particles.json', function() {
+		console.log('callback - particles.js config loaded');
+});
+
+});
