9f2f6b4fd0ff433b9cf5d578119121c09c5eb4f4
volitilov <volitilov@gmail.com>
2018-02-11 15:56:21 +0300

    v1

---
+++ works/wood_son/demo/README.md
@@ -0,0 +1,18 @@
+# Greenside. 
+###### preview: [greenside](https://volitilov.github.io/works/greenside/index.html)
+
+Greenside - это шаблон HTML-страницы на одной странице, построенный на базе Bootstrap. Шаблон идеально подходит для озеленения и озеленения компаний, услуг по уходу за газонами, ландшафтных архитекторов, земляных мастеров и других. Особенности включают полностью отзывчивый макет, анимацию CSS3, фильтруемую галерею, раздел отзывов, рабочую форму контакта и многое другое.
+
+
+#### технологии:
+
+* `css` 
+* `html` 
+* `js` 
+* `wow.js` 
+* `svg` 
+* `gif` 
+* `animations` 
+* `responsive`
+
+![scrin image](https://volitilov.github.io/works/greenside/img/greenside_s.png)
---
+++ works/wood_son/demo/js/main.js
@@ -0,0 +1,112 @@
+
+function main() {
+
+(function () {
+   'use strict';
+   
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 50
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+	
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+	
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+	
+		// Preloader
+  	$(window).load(function() {
+
+   	// will first fade out the loading animation 
+    	$("#status").fadeOut("slow"); 
+
+    	// will fade out the whole DIV that covers the website. 
+    	$("#preloader").delay(500).fadeOut("slow").remove();      
+
+  	});
+	
+    // Nivo Lightbox 
+    $('.portfolio-item a').nivoLightbox({
+            effect: 'slideDown',  
+            keyboardNav: true,                            
+        });
+		
+	// Testimonial Slider
+	  	$(document).ready(function() {
+	      $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});	
+
+ 	// WOW JS
+	$(window).load(function() {
+	new WOW().init();
+	//smoothScroll
+	smoothScroll.init();
+		});
+
+}());
+
+
+}
+main();
